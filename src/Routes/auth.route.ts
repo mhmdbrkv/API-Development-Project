f222872b0b8b23c982dc53f0d7a0d08b3b3d4b54
mhmdbrkv <barakamohamed964@gmail.com>
2024-10-25 22:56:00 +0300

    adding auth middleware and organization endpoints

--- src/Routes/auth.route.ts
+++ src/Routes/auth.route.ts
@@ -6,9 +6,10 @@ import {
   signin,
   refreshToken,
 } from "../Controllers/auth.controller.js";
+import { guard } from "../Middleware/auth.middleware.js";
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post("/refresh-token", refreshToken);
+router.post("/refresh-token", guard, refreshToken);
 
 export default router;
--- src/Routes/organization.route.ts
+++ src/Routes/organization.route.ts
@@ -2,16 +2,21 @@ import express from "express";
 const router = express.Router();
 
 import {
-  signup,
-  signin,
-  refreshToken,
-} from "../Controllers/auth.controller.js";
+  createOne,
+  getMany,
+  getOne,
+  updateOne,
+  deleteOne,
+  inviteMember,
+} from "../Controllers/organization.controller.js";
+import { allowedTo, guard } from "../Middleware/auth.middleware.js";
 
-router.post("/organization", signup);
-router.get("/organization", signin);
-router.get("/organization/:organization_id", signin);
-router.put("/organization/:organization_id", signin);
-router.delete("/organization/:organization_id", signin);
-router.post("/organization/:organization_id/invite", signin);
+router.use(guard, allowedTo("admin"));
+router.post("/organization", createOne);
+router.get("/organization", getMany);
+router.get("/organization/:organization_id", getOne);
+router.put("/organization/:organization_id", updateOne);
+router.delete("/organization/:organization_id", deleteOne);
+router.post("/organization/:organization_id/invite", inviteMember);
 
 export default router;
