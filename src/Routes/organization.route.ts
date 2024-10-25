a568db7b969c7806c60e1f4c00c64520116f255b
mhmdbrkv <barakamohamed964@gmail.com>
2024-10-25 22:57:46 +0300

    adding invited user read-only access to the organization's data

--- src/Routes/organization.route.ts
+++ src/Routes/organization.route.ts
@@ -11,12 +11,16 @@ import {
 } from "../Controllers/organization.controller.js";
 import { allowedTo, guard } from "../Middleware/auth.middleware.js";
 
-router.use(guard, allowedTo("admin"));
-router.post("/organization", createOne);
+router.use(guard);
+router.post("/organization", allowedTo("admin"), createOne);
 router.get("/organization", getMany);
 router.get("/organization/:organization_id", getOne);
-router.put("/organization/:organization_id", updateOne);
-router.delete("/organization/:organization_id", deleteOne);
-router.post("/organization/:organization_id/invite", inviteMember);
+router.put("/organization/:organization_id", allowedTo("admin"), updateOne);
+router.delete("/organization/:organization_id", allowedTo("admin"), deleteOne);
+router.post(
+  "/organization/:organization_id/invite",
+  allowedTo("admin"),
+  inviteMember
+);
 
 export default router;
